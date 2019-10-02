70956e8b8d7d2f1127932ed5edae33b6c6b245a6
Dali Zheng <d@liwa.li>
2019-10-02 08:41:50 -0700

    fix deprecation warning in test

--- test/server.js
+++ test/server.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (request, response) {
     path.join(__dirname, '../coverage.json')))
   request.on('end', function () {
     response.setHeader('Access-Control-Allow-Origin', '*')
-    response.end(new Buffer(0), function () { server.close() })
+    response.end('', function () { server.close() })
   })
 })
 
